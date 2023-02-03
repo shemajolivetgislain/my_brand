1cf68fad3de3f7bb71171940eaf7ec3b7f221d1c
shema jolivet gislain <shemajolivet@gmail.com>
2023-02-03 13:15:20 +0200

    login and dashboard design

--- main.js
+++ main.js
@@ -2,3 +2,36 @@ const header = document.querySelector('header');
 window.addEventListener('scroll',  function () {
     header.classList.toggle('sticky', window.scrollY > 200);
 });
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.nav-list');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+windows.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+
+$(document).ready(function() {
+    $('.filter-item').click(function(){
+        const value = $(this).attr('data-filter');
+        if(value == 'all'){
+            $('.post-box').show('1000');
+        } else{
+            $('.post-box').not('.'+value).hide('1000');
+            $('.post-box').filter('.'+value).show('1000');
+        }
+    
+    });
+    $('.filter-item').click(function(){
+        $(this).addClass('active-filter').siblings().removeClass('active-filter');
+    
+    });
+
+});
+
