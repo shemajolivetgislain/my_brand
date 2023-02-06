7f15cdd9f6ae673ffc676456723689621c349de4
shema jolivet gislain <shemajolivet@gmail.com>
2023-02-06 23:07:41 +0200

    Fixing navbar responsive

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const header = document.querySelector('header');
 window.addEventListener('scroll',  function () {
-    header.classList.toggle('sticky', window.scrollY > 200);
+    header.classList.toggle('sticky', window.scrollY > 100);
 });
 
 let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.nav-list');
+let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
