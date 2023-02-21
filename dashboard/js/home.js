ba6bfd60f0f750fb5c0576459952536744c79ed9
shema jolivet gislain <shemajolivet@gmail.com>
2023-02-21 08:33:23 +0200

    user role management

--- dashboard/js/home.js
+++ dashboard/js/home.js
@@ -46,3 +46,22 @@ function displayBlogs() {
 
 // call the displayBlogs function to populate the table
 displayBlogs();
+
+
+const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+const adminMenu = Array.from(document.getElementsByClassName("admin"));
+const userMenu = Array.from(document.getElementsByClassName("user"));
+
+// if(currentUser.role != "admin") adminMenu.forEach(e => e.style.display = 'block');
+// if(currentUser.role != "user") userMenu.forEach(e => e.style.display = 'none');
+
+if(currentUser.role === "admin") {
+  adminMenu.forEach(e => e.style.display = 'block');
+  userMenu.forEach(e => e.style.display = 'block');
+  } 
+  else if(currentUser.role === "user") {
+  adminMenu.forEach(e => e.style.display = 'none');
+  userMenu.forEach(e => e.style.display = 'block');
+  } 
+  
---
+++ dashboard/js/userroles.js
@@ -0,0 +1,17 @@
+const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+const adminMenu = Array.from(document.getElementsByClassName("admin"));
+const userMenu = Array.from(document.getElementsByClassName("user"));
+
+// if(currentUser.role != "admin") adminMenu.forEach(e => e.style.display = 'block');
+// if(currentUser.role != "user") userMenu.forEach(e => e.style.display = 'none');
+
+if(currentUser.role === "admin") {
+  adminMenu.forEach(e => e.style.display = 'block');
+  userMenu.forEach(e => e.style.display = 'block');
+  } 
+  else if(currentUser.role === "user") {
+  adminMenu.forEach(e => e.style.display = 'none');
+  userMenu.forEach(e => e.style.display = 'block');
+  } 
+  
