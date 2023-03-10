20300ca743e7c7899e35c90a8768f6379f66d8a2
shema jolivet gislain <shemajolivet@gmail.com>
2023-03-10 16:22:18 +0200

    adding the delte function delete functionality

--- dashboard/js/displayblog.js
+++ dashboard/js/displayblog.js
@@ -1,3 +1,25 @@
+// delete article
+function deleteArticle(id, tr) {
+  console.log("Deleting blog post with ID: " + id);
+  let authToken = JSON.parse(localStorage.getItem("authToken"));
+  fetch(`https://my-brand-api-wm4u.onrender.com/api/blogs/delete/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: `Bearer ${authToken.token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // remove table row after deleting the blog post
+      tr.remove();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
 // display blogs in the table
 function displayBlogs() {
   const blogsTable = document.querySelector("tbody");
@@ -13,33 +35,34 @@ function displayBlogs() {
           const tr = document.createElement("tr");
           const createdAt = new Date(blog.createdAt);
           tr.innerHTML = `
-        <td class="blog-title">
-        <p>${i + 1}</p>
-        </td>
+            <td class="blog-title">
+              <p>${i + 1}</p>
+            </td>
 
-        <td class="picture">
-          <img src="${blog.image}" alt="">
-        </td>
-        <td class="blog-title">
-          <p>${blog.title}</p>
-        </td>
-        <td class="author">
-          <p>${blog.category}</p>
-        </td>
-        <td class="date">
-        
-          <p>${createdAt.getDate()}-${
+            <td class="picture">
+              <img src="${blog.image}" alt="">
+            </td>
+            <td class="blog-title">
+              <p>${blog.title}</p>
+            </td>
+            <td class="author">
+              <p>${blog.category}</p>
+            </td>
+            <td class="date">
+              <p>${createdAt.getDate()}-${
             createdAt.getMonth() + 1
           }-${createdAt.getFullYear()}</p>
-        </td>
-        <td class="status">
-          <p>${blog.statuse}</p>
-        </td>
-        <td class="action">
-          <a href="#"><i class='bx bxs-edit'></i></a>
-          <a href="#"><i class='bx bxs-trash'></i></a>
-        </td>
-      `;
+            </td>
+            <td class="status">
+              <p>${blog.statuse}</p>
+            </td>
+            <td class="action">
+              <a href="#"><i class='bx bxs-edit'></i></a>
+              <a href="#"><i class='bx bxs-trash' onclick="deleteArticle('${
+                blog._id
+              }', this.parentNode.parentNode)"></i></a>
+            </td>
+          `;
           blogsTable.appendChild(tr);
         });
       } else {
