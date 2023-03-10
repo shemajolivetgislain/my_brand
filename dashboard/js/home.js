97b562ef4bb81c0c055330f6f44100b04681d2b4
shema jolivet gislain <shemajolivet@gmail.com>
2023-03-10 16:41:36 +0200

    retrieving the login user detail

--- dashboard/js/home.js
+++ dashboard/js/home.js
@@ -1,3 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const currentlogin = document.getElementById("currentLogin");
+  const authToken = JSON.parse(localStorage.getItem("authToken"));
+  console.log(authToken.username);
+  currentlogin.innerHTML = `${authToken.username}`;
+});
+
 function displayBlogs() {
   const blogsTable = document.querySelector("tbody");
   blogsTable.innerHTML = "";
